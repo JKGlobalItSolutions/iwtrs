7c3195eedf3c58a36ce5723e1db41cc942c5ee0c
sudhar-dev-02 <sudharsanp300@gmail.com>
2026-05-27 16:02:48 +0530

    /

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base:"/iwtrs/",
+  base:"/",
   plugins: [react()],
   test: {
     environment: "jsdom",
